f1042af925ec49fc74857e3dcfdd3147d4f9a7eb
ekrem-qb <ekrem.bayram@mail.ru>
2022-08-08 23:45:29 +0300

    first commit

---
+++ ImageViewer/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ImageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.PictureBox1_DoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Images|*.png;*.jpg;*.jpeg;*.bmp";
+            this.openFileDialog1.ReadOnlyChecked = true;
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.5D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseWheel);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ImageViewer/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        Image image;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ImageLoad()
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+            }
+            catch (FileNotFoundException)
+            {
+            }
+
+            try
+            {
+                image = Image.FromFile(openFileDialog1.FileName);
+
+                pictureBox1.Image = image;
+
+                timer1.Start();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Select an image file");
+                Application.Exit();
+            }
+
+            Point screenCenter = new Point((Screen.PrimaryScreen.WorkingArea.Width / 2) - (pictureBox1.Width / 2), (Screen.PrimaryScreen.WorkingArea.Height) / 2 - (pictureBox1.Height / 2));
+            pictureBox1.Location = screenCenter;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ImageLoad();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Width == image.Width)
+            {
+                timer1.Stop();
+            }
+            else if (pictureBox1.Width < image.Width)
+            {
+                pictureBox1.Width += 2;
+
+                pictureBox1.Left --;
+            }
+            else if (pictureBox1.Width > image.Width)
+            {
+                pictureBox1.Width -= 2;
+
+                pictureBox1.Left ++;
+            }
+
+            if (pictureBox1.Height == image.Height)
+            {
+                timer1.Stop();
+            }
+            else if (pictureBox1.Height < image.Height)
+            {
+                pictureBox1.Height += 2;
+
+                pictureBox1.Top --;
+            }
+            else if (pictureBox1.Height > image.Height)
+            {
+                pictureBox1.Height -= 2;
+
+                pictureBox1.Top ++;
+            }
+        }
+
+
+            /*-------Window Dragging-------
+
+            protected override void WndProc(ref Message m)
+            {
+                switch (m.Msg)
+                {
+                    case 0x84:
+                        base.WndProc(ref m);
+                        if ((int)m.Result == 0x1)
+                            m.Result = (IntPtr)0x2;
+                        return;
+                }
+
+                base.WndProc(ref m);
+            }*/
+
+            private void Form1_MouseWheel(object sender, MouseEventArgs e)
+            {
+                if (e.Delta < 0)
+                {
+                    pictureBox1.Height -= (int)(image.Height * 0.05);
+                    pictureBox1.Width -= (int)(image.Width * 0.05);
+
+                    pictureBox1.Top += (int)(image.Height * 0.025);
+                    pictureBox1.Left += (int)(image.Width * 0.025);
+                }
+                else
+                {
+                    pictureBox1.Height += (int)(image.Height * 0.05);
+                    pictureBox1.Width += (int)(image.Width * 0.05);
+
+                    pictureBox1.Top -= (int)(image.Height * 0.025);
+                    pictureBox1.Left -= (int)(image.Width * 0.025);
+                }
+            }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        bool isDragging = false;
+        int currentX, currentY;
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Cursor = Cursors.Cross;
+
+                pictureBox1.Top += (e.Y - currentY);
+                pictureBox1.Left += (e.X - currentX);
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+            Cursor = Cursors.Default;
+        }
+
+        private void PictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            ImageLoad();
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+
+            currentX = e.X;
+            currentY = e.Y;
+        }
+    }
+}
